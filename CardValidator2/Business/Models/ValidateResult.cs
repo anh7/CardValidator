2b0343e5990795c54712bce8a4a28743354ef98e
Anh Truong <hoanganh727018@gmail.com>
2018-06-19 22:25:28 +0700

    Added basic components

---
+++ CardValidator2/Business/Models/ValidateResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CardValidator2.Business.Models
+{
+    public class ValidateResult
+    {
+        public Result Result { get; set; }
+        public string CardType { get; set; }
+    }
+
+    public enum Result
+    {
+        Valid = 1,
+        Invalid,
+        NotExist
+    }
+}
---
+++ CardValidator2/Controllers/CardController.cs
@@ -0,0 +1,114 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CardValidator2.Models;
+
+namespace CardValidator2.Controllers
+{
+    public class CardController : ApiController
+    {
+        private CardValidatorEntities db = new CardValidatorEntities();
+
+        // GET: api/Card
+        public IQueryable<Card> GetCards()
+        {
+            return db.Cards;
+        }
+
+        // GET: api/Card/5
+        [ResponseType(typeof(Card))]
+        public IHttpActionResult GetCard(int id)
+        {
+            Card card = db.Cards.Find(id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(card);
+        }
+
+        // PUT: api/Card/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCard(int id, Card card)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != card.CardId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(card).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Card
+        [ResponseType(typeof(Card))]
+        public IHttpActionResult PostCard(Card card)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Cards.Add(card);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = card.CardId }, card);
+        }
+
+        // DELETE: api/Card/5
+        [ResponseType(typeof(Card))]
+        public IHttpActionResult DeleteCard(int id)
+        {
+            Card card = db.Cards.Find(id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            db.Cards.Remove(card);
+            db.SaveChanges();
+
+            return Ok(card);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CardExists(int id)
+        {
+            return db.Cards.Count(e => e.CardId == id) > 0;
+        }
+    }
+}
